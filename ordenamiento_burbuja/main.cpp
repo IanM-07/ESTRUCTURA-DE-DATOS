218fc45237992155c5c265c5b645335263e4b0d4
Ian Luis Morales Canales <ianchipotinluis31@gmail.com>
2026-09-21 10:49:00 -0600

    feat: agrega seguimiento de pasadas y contadores

--- ordenamiento_burbuja/main.cpp
+++ ordenamiento_burbuja/main.cpp
@@ -3,18 +3,37 @@ using namespace std;
 
 void burbuja(int numeros[], int n)
 {
+    int comparaciones = 0;
+    int intercambios = 0;
+
     for (int i = 0; i < n - 1; i++)
     {
         for (int j = 0; j < n - 1 - i; j++)
         {
+            comparaciones++;
+
             if (numeros[j] > numeros[j + 1])
             {
                 int temporal = numeros[j];
                 numeros[j] = numeros[j + 1];
                 numeros[j + 1] = temporal;
+
+                intercambios++;
             }
         }
+
+        cout << "Pasada " << i + 1 << ": ";
+
+        for (int j = 0; j < n; j++)
+        {
+            cout << numeros[j] << " ";
+        }
+
+        cout << endl;
     }
+
+    cout << "Comparaciones: " << comparaciones << endl;
+    cout << "Intercambios: " << intercambios << endl;
 }
 
 int main()
@@ -29,9 +48,11 @@ int main()
         cout << numeros[i] << " ";
     }
 
+    cout << endl;
+
     burbuja(numeros, n);
 
-    cout << "\nArreglo ordenado: ";
+    cout << "Arreglo ordenado: ";
 
     for (int i = 0; i < n; i++)
     {
